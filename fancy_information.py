b38bd797dbc004458b1d148cf063228772c84b8d
Arvie Lastra <lastraarvie@gmail.com>
2024-03-23 23:34:02 +0800

    Add tprint

--- fancy_information.py
+++ fancy_information.py
@@ -7,3 +7,6 @@ dream_job = input("What is your Dream job?: ")
 
 # concatenate the users input
 users_info = "Name: " + name + '\n' + "Dream job: " + dream_job
+
+# print using "tprint" function from the art module
+tprint(users_info, "xlarge-ferhe454u4y4y")
