d8bc20f3399166acc41f9dbe5aaa299cf3af5f76
Arvie Lastra <lastraarvie@gmail.com>
2024-03-23 23:26:50 +0800

    Add concatenate

--- fancy_information.py
+++ fancy_information.py
@@ -4,3 +4,6 @@ from art import *
 # Add users input
 name = input("What is your Name?: ")
 dream_job = input("What is your Dream job?: ")
+
+# concatenate the users input
+users_info = "Name: " + name + '\n' + "Dream job: " + dream_job
